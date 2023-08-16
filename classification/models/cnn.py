5fea5e830641d3538ff2821dc5783d24fcd2259b
sunwoo <sunsense9@gmail.com>
2023-08-16 21:58:12 +0900

    cnn 레이어 수정

--- classification/models/cnn.py
+++ classification/models/cnn.py
@@ -1,35 +1,30 @@
-from torch import nn
+import torch.nn as nn
 
 
 class ConvolutionBlock(nn.Module):
-    
+
     def __init__(self, 
                  input_channels, 
                  output_channels):
-        self.input_channels = input_channels
-        self.output_channels = output_channels
 
         super().__init__()
 
         self.layers = nn.Sequential(
             nn.Conv2d(input_channels, output_channels, (3, 3), padding=1), 
-            nn.LeakyReLU(), 
+            nn.ReLU(), 
             nn.BatchNorm2d(output_channels), 
             nn.Conv2d(output_channels, output_channels, (3, 3), stride=2, padding=1), 
-            nn.LeakyReLU(), 
+            nn.ReLU(), 
             nn.BatchNorm2d(output_channels)
         )
 
-    def forward(self, x):
-        y = self.layers(x)
-        
-        return y
+    def forward(self, x):        
+        return self.layers(x)
     
 
 class CNNClassifier(nn.Module):
     
     def __init__(self, output_size):
-        self.output_size = output_size
         
         super().__init__()
 
@@ -43,18 +38,15 @@ class CNNClassifier(nn.Module):
 
         self.layers = nn.Sequential(
             nn.Linear(512, 256), 
-            nn.LeakyReLU(), 
+            nn.ReLU(), 
             nn.BatchNorm1d(256), 
             nn.Linear(256, 128), 
-            nn.LeakyReLU(), 
+            nn.ReLU(), 
             nn.BatchNorm1d(128), 
             nn.Linear(128, 64), 
-            nn.LeakyReLU(), 
+            nn.ReLU(), 
             nn.BatchNorm1d(64), 
-            nn.Linear(64, 32), 
-            nn.LeakyReLU(), 
-            nn.BatchNorm1d(32), 
-            nn.Linear(32, output_size), 
+            nn.Linear(64, output_size), 
             nn.LogSoftmax(dim=-1)
         )
 
@@ -65,6 +57,4 @@ class CNNClassifier(nn.Module):
             x = x.view(-1, 1, x.size(-2), x.size(-1))
             
         z = self.blocks(x)
-        y = self.layers(z.squeeze())
-
-        return y
+        return self.layers(z.squeeze())
